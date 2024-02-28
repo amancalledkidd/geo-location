0f393f5a9255672bd07e8abe029d4aabba5aab27
Kumani K <kumanikidd@gmail.com>
2024-02-28 14:36:42 +0000

    greeting added to home. dynamically changes based on time

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,28 @@
 import WeatherInfo from '../WeatherInfo/WeatherInfo'
 import './Home.scss'
+import { useEffect, useState } from 'react'
 
 
 
 const Home = () => {
+    const [greeting, setGreeting] = useState<string>('')
 
+    useEffect(() => {
+        const date = new Date()
+        const hours = date.getHours()
+        if (hours < 12) {
+            setGreeting('Good morning')
+        } else if (hours < 18) {
+            setGreeting('Good afternoon')
+        } else {
+            setGreeting('Good evening')
+        }
+    }, [])
 
     return (
         <div className="home">
             <h1 className='home__title'>Home</h1>
+            <h1 className='home__greeting'>{greeting} Charlotte</h1>
             <WeatherInfo />
         </div>
     )
