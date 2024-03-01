7eb409d0899a9d9bb5fe92aa5d9d3bc1fe352658
Kumani K <kumanikidd@gmail.com>
2024-03-01 18:54:12 +0000

    updated todo container and added styling

--- src/container/TodoContainer/TodoContainer.tsx
+++ src/container/TodoContainer/TodoContainer.tsx
@@ -1,18 +1,19 @@
 import './TodoContainer.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodoList from '../../components/TodoList/TodoList'
 import { Todo } from '../../types/Todo'
 import TodoInput from '../../components/TodoInput/TodoInput'
 
 const TodoContainer = () => {
     const [todos, setTodos] = useState<Todo[]>([])
-    const [newTodo, setNewTodo] = useState<Todo>({
-        name: '',
-        description: '',
-        completed: false
-    })
 
-    const handleUpdateTodos = () => {
+    useEffect(() => {
+        console.log(todos)
+    }
+    , [todos])
+
+
+    const handleUpdateTodos = (newTodo: Todo) => {
         setTodos([...todos, newTodo])
     }
 
@@ -20,8 +21,8 @@ const TodoContainer = () => {
 
     return (
         <div className="todo-container">
-            <h1 className="todo-container__title">Todo Container</h1>
-            <TodoInput setNewTodo={setNewTodo} updateTodos={handleUpdateTodos} />
+            <h1 className="todo-container__title">Today's Tasks</h1>
+            <TodoInput updateTodos={handleUpdateTodos} />
             <TodoList todos={todos} />
             
         </div>
