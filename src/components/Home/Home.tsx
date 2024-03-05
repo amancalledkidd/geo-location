d458d88503e6ab1f08f581fe939476e65e81a49d
Kumani K <kumanikidd@gmail.com>
2024-03-05 10:09:12 +0000

    Layout updates, location useState move home to be used in multiple child elements

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,29 +1,58 @@
 import WeatherInfo from '../WeatherInfo/WeatherInfo'
 import './Home.scss'
 import { useEffect, useState } from 'react'
+import Clock from '../Clock/Clock'
+import TodoContainer from '../../container/TodoContainer/TodoContainer'
+import { GeoLocation } from '../../types/GeoLocation'
+import CustomMap from '../CustomMap/CustomMap'
 
 
 
 const Home = () => {
     const [greeting, setGreeting] = useState<string>('')
+    const [location, setLocation] = useState<GeoLocation>()
+    const user = 'Kumani'
+
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+            setLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            })
+        })
+        } else {
+            console.log('Geolocation is not supported by this browser.')
+        }
+    }, [])
 
     useEffect(() => {
         const date = new Date()
         const hours = date.getHours()
         if (hours < 12) {
-            setGreeting('Good morning')
+            setGreeting('Good Morning')
         } else if (hours < 18) {
-            setGreeting('Good afternoon')
+            setGreeting('Good Afternoon')
         } else {
-            setGreeting('Good evening')
+            setGreeting('Good Evening')
         }
     }, [])
 
     return (
         <div className="home">
-            <h1 className='home__title'>Home</h1>
-            <h1 className='home__greeting'>{greeting} Charlotte</h1>
-            <WeatherInfo />
+            <h1 className='home__greeting'>{greeting}, {user}!</h1>
+            <div className="home__top-row">
+                <Clock />
+                <WeatherInfo location={location}/>
+                <CustomMap location={location}/>
+            </div>
+            <div className="home__bottom-row">
+                <TodoContainer />
+            </div>
+
+            
+            
         </div>
     )
 }
