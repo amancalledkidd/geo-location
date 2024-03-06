2809a65516e150c2aa6425585e12d9f28d88d1d1
Kumani K <kumanikidd@gmail.com>
2024-03-06 11:36:09 +0000

    journal card setup

---
+++ src/components/JournalCard/JournalCard.tsx
@@ -0,0 +1,12 @@
+import './JournalCard.scss'
+
+const JournalCard = () => {
+    return (
+        <div className="journal-card">
+            <h1 className="journal-card__title">Journal Card</h1>
+        </div>
+    )
+}
+
+export default JournalCard
+
