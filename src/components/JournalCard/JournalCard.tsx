756c1c5c6e6a03c18f12daa913e52b9221be0159
Kumani K <kumanikidd@gmail.com>
2024-03-07 11:04:37 +0000

    journal component added!

--- src/components/JournalCard/JournalCard.tsx
+++ src/components/JournalCard/JournalCard.tsx
@@ -1,9 +1,34 @@
 import './JournalCard.scss'
+import { useState, ChangeEvent } from 'react'
+
 
 const JournalCard = () => {
+    const [entry, setEntry] = useState<string>('')
+    const [edit, setEdit] = useState<boolean>(true)
+
+    const editJournalClick = () => {
+        console.log('Journal entry submitted')
+        setEdit(!edit)
+    }
+    const handleEntryChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setEntry(e.target.value)
+    }
+
     return (
         <div className="journal-card">
-            <h1 className="journal-card__title">Journal Card</h1>
+            <h4 className='journal-card__title'>Journal</h4>
+            <textarea
+                className="journal-card__entry"
+                value={entry}
+                placeholder="Today's entry" 
+                onChange={handleEntryChange}
+                disabled={!edit} 
+                />
+            {!edit ? <button className="journal-card__edit" onClick={editJournalClick}>Edit</button> : 
+            <button className="journal-card__submit"  onClick={editJournalClick}>Submit</button>
+            }   
+
+
         </div>
     )
 }
