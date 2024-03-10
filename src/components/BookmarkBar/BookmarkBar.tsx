3ee24d778d218fda8a3fe99f805639c6e9a06615
Kumani K <kumanikidd@gmail.com>
2024-03-10 20:41:29 +0000

    bookmark bar added for top of home page

---
+++ src/components/BookmarkBar/BookmarkBar.tsx
@@ -0,0 +1,11 @@
+import './BookmarkBar.scss'
+
+const BookmarkBar = () => {
+    return (
+        <div className="bookmark-bar">
+            <h1>Bookmark Bar</h1>
+        </div>
+    )
+}
+
+export default BookmarkBar
